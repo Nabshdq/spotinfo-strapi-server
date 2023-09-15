4de387200c05fc0eea2710f3f559ca49be923cc6
NaaoiCode <muhnabil7604@gmail.com>
2023-09-15 21:47:31 +0800

    Initial commit

--- config/plugin.js
+++ config/plugin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
     // ...
+    "vercel-deploy": {
+        enabled: true,
+    },
     upload: {
         config: {
             provider: 'cloudinary',
@@ -16,3 +19,17 @@ module.exports = ({ env }) => ({
     },
     // ...
 });
+
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+        enabled: true,
+        config: {
+            deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+            apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+            appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+            teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+            roles: ["strapi-super-admin"],
+        },
+    },
+});
+
